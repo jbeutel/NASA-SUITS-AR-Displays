fb733dba561e23a34f3e0d8c30cd6e32154dbba2
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-02-06 13:13:51 -0500

    got mapbox working

--- Assets/Scripts/deub.cs
+++ Assets/Scripts/deub.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class deub : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject heartratebutton;
+    
+    public void buttonpressed()
     {
-        
+        Debug.Log("Button hit");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
